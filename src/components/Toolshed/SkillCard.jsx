0598ce1bc8907cd2ed9f0b51fcf6b49cd90fcc6a
Baranuel <samuel.baran98@gmail.com>
2022-07-23 12:58:19 +0200

    moved gsap animation to the parent

--- src/components/Toolshed/SkillCard.jsx
+++ src/components/Toolshed/SkillCard.jsx
@@ -1,55 +1,10 @@
 import React, {useEffect, useState} from 'react'
-import gsap from 'gsap'
-import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
 
 function SkillCard(props) {
   
   const {image, title, skills} = props
 
-
-  useEffect( () => {
-
-    const cardsCover = gsap.utils.toArray('.skill-card-cover')
-    const cardsContent = gsap.utils.toArray('.skill-card-content')
-    gsap.registerPlugin(ScrollTrigger);
-
-
-    cardsCover.forEach(card => {
-      gsap.to(card, {
-        scrollTrigger: {
-          trigger: card,
-          start:"top 60%",
-          scrub:true,
-          end:"top 60%"
-        },
-        ease:"ease",
-        x:"0%",
-        duration: 0.3,
-        boxShadow: "4px 0 8px -2px rgb(24, 24, 24)"
-      })
-    })
-
-    cardsContent.forEach(card => {
-      gsap.from(card, {
-        x:"-70%",
-
-      })
-      gsap.to(card, {
-        scrollTrigger: {
-          trigger: card,
-          start:"top 60%",
-          scrub:true,
-          end:"top 60%"
-        },
-        ease:"ease-in",
-        x:"-5%",
-
-
-      })
-    })
-
-
-  },[])
 
  
 
--- src/components/Toolshed/Toolshed.jsx
+++ src/components/Toolshed/Toolshed.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import gsap from 'gsap'
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
 import SkillCard from './SkillCard'
 
 import cardCover1 from '../../assets/images/card-cover-1.svg'
@@ -7,6 +10,50 @@ import cardCover2 from '../../assets/images/card-cover-2.svg'
 import skills from '../../skills'
 
 function Toolshed() {
+
+  useEffect( () => {
+
+    const cardsCover = gsap.utils.toArray('.skill-card-cover')
+    const cardsContent = gsap.utils.toArray('.skill-card-content')
+    gsap.registerPlugin(ScrollTrigger);
+
+
+    cardsCover.forEach(card => {
+      gsap.to(card, {
+        scrollTrigger: {
+          trigger: card,
+          start:"top 60%",
+          scrub:true,
+          end:"top 60%"
+        },
+        ease:"ease",
+        x:"0%",
+        duration: 0.3,
+        boxShadow: "4px 0 8px -2px rgb(24, 24, 24)"
+      })
+    })
+
+    cardsContent.forEach(card => {
+      gsap.from(card, {
+        x:"-70%",
+
+      })
+      gsap.to(card, {
+        scrollTrigger: {
+          trigger: card,
+          start:"top 60%",
+          scrub:true,
+          end:"top 60%"
+        },
+        ease:"ease-in",
+        x:"-5%",
+
+
+      })
+    })
+
+
+  },[])
 
 
   return (
