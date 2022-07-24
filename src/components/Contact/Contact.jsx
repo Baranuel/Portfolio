4b9921be74309365f66288b7fe15eaa20b4fdeb7
Baranuel <samuel.baran98@gmail.com>
2022-07-24 20:23:36 +0200

    contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import emailjs from '@emailjs/browser'
 
 import orangeSvg from '../../assets/images/orange-svg.svg'
 import purpleSvg from '../../assets/images/purple-svg.svg'
 
 function Contact() {
+    console.log(emailjs)
+    const [contactFormData, setContactFormData] = useState({})
+    const[submited, setSubmited] = useState(false)
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        emailjs.send((process.env.REACT_APP_SERVICE_ID), (process.env.REACT_APP_TEMPLATE_ID),contactFormData, (process.env.REACT_APP_USER_ID) )
+                .then((response) => {
+                    console.log("SUCCESS", response);
+                    setContactFormData({})
+
+                    },
+                    err => {
+                        console.log("FAILED...", err)
+                    })
+
+        setContactFormData( ({
+            name:"",
+            email:"",
+            subject:"",
+            message:""
+        }))
+    }
+
+
+    const handleChange = e => {
+
+        console.log(e.target.value)
+
+        setContactFormData(prev => ({
+            ...prev,
+            [e.target.name]:e.target.value
+        }))
+    }
+
+    console.log(contactFormData)
+    console.log(process.env.REACT_APP_SERVICE_ID)
+
   return (
     <div className='front-page-contact'>
         <div className="contact-wrapper">
@@ -18,15 +58,16 @@ function Contact() {
                 </div>
             </div>
             <div className="contact-form">
-                <form action="">
-                    <label for="name" > Full Name </label>
-                    <input name="name" className='input-short' type="text" placeholder='Full Name' />
-                    <label for="email" > Email </label>
-                    <input name="email" className='input-short' type="text" placeholder='Email' />
-                    <label for="subject" > Subject </label>
-                    <input className='input-short' type="text" placeholder='Subject' />
-                    <label for="message" > Message </label>
-                    <textarea name='message' className='input-area' />
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="name" > Full Name </label>
+                    <input onChange={handleChange} name="name" value={contactFormData.name} className='input-short' type="text" placeholder='Full Name' />
+                    <label htmlFor="email" > Email </label>
+                    <input onChange={handleChange} name="email" value={contactFormData.email} className='input-short' type="text" placeholder='Email' />
+                    <label htmlFor="subject" > Subject </label>
+                    <input onChange={handleChange} name="subject" value={contactFormData.subject} className='input-short' type="text" placeholder='Subject' />
+                    <label htmlFor="message" > Message </label>
+                    <textarea  onChange={handleChange} name='message' value={contactFormData.message} className='input-area' />
+                <button className='submit-button primary-button'>Contact me</button>
                 </form>
             </div>
 
